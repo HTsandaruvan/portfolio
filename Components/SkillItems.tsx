e16ade79d1ce98fc9be36b6ed3087e741eec7c6a
Harsha Tharuka <108293334+HTsandaruvan@users.noreply.github.com>
2024-11-19 23:01:48 +0530

    update skills

--- Components/SkillItems.tsx
+++ Components/SkillItems.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
-const SkillItems = () => {
+interface Props{
+    title: string
+    year:string
+}
+const SkillItems = ({title,year} :Props) => {
   return (
-    <div>SkillItems</div>
+    <div className='mb-[4rem] md:mb-[8rem]'>
+        <span className='px-[2rem] text-[#55e6a5] py-[0.9rem] font-bold text-[18px] border-[2px] border-[#55e6a5]'>
+            {year}
+        </span>
+        <h1 className=' mt-[2rem] uppercase font-semibold mb-[2rem] text-[20px] sm:text-[25px] md:text-[30px] text-white'>
+            {title}
+        </h1>
+        <p className='text-[#aaaaaa] font-normal w-[80%] text-[17px] opacity-80'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Quibusdam impedit voluptatum voluptas, corporis cumque at 
+            maiores quod? Non et aspernatur eaque illum id beatae fugiat voluptas quasi, iusto, ab facilis.
+        </p>
+    </div>
   )
 }
 
--- Components/Skills.tsx
+++ Components/Skills.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import SkillItems from './SkillItems'
+import SkillLanguages from './SkillLanguages'
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div className='pt-[4rem] md:pt-[8rem] pb-[5rem] bg-[#09101a]'>
+        <h1 className='heading'>
+            Education & <span className='text-yellow-400'>Skills</span>
+        </h1>
+        <div className='w-[80%] mx-auto pt-[4rem] md:pt-[8rem] grid-cols-1 md:grid-cols-2 gap-[2rem] items-center'>
+            <div>
+                <SkillItems title="React Developer" year="2014 - 2018"/>
+                <SkillItems title="Mern Stack Developer" year="2018 - 2019"/>
+                <SkillLanguages skill1="HTML" skill2="CSS" skill3="JavaScript" level1="w-[91%]" level2="w-[80%]" level3="w-[80%]"/>
+            </div>
+            <div>
+                <SkillItems title="Next Js Developer" year="2020 - 2021"/>
+                <SkillItems title="Node Js Developer" year="2022 - 2024"/>
+                <SkillLanguages skill1="Next Js" skill2="React Js" skill3="TypeScript" level1="w-[85%]" level2="w-[900%]" level3="w-[95%]"/>
+            </div>
+        </div>
+    </div>
   )
 }
 
