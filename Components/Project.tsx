57881705877f8e2932c5009551db6984a9a4e116
Harsha Tharuka <108293334+HTsandaruvan@users.noreply.github.com>
2024-11-22 21:47:12 +0530

    new branch

--- Components/Project.tsx
+++ Components/Project.tsx
@@ -23,7 +23,7 @@ const Project = () => {
             </div>
             <div>
                 <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
-                    <Image src="/images/projects/project-1.jpeg" alt="portfolio" layout="fill" className='object-contain'/>
+                    <Image src="/images/projects/project-1.png" alt="portfolio" layout="fill" className='object-contain'/>
                 </div>
             </div>
             <div>
